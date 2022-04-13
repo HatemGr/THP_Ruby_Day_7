b181680fc75a7fb9cad56e37bf0ae6e6fe24e1e1
HatemGr <103052216+HatemGr@users.noreply.github.com>
2022-04-13 08:01:36 +0200

    Update exo_14.rb

--- exo_14.rb
+++ exo_14.rb
@@ -2,9 +2,9 @@ mail_list = []
 
 50.times {|i| 
 if i < 9 
-    mail_list.push("jean.dupont0#{i+1}@email.fr")
+    mail_list.push("jean.dupont.0#{i+1}@email.fr")
  else
-  mail_list.push("jean.dupont#{i+1}@email.fr")
+  mail_list.push("jean.dupont.#{i+1}@email.fr")
  end}
 
 mail_list_pair = []
